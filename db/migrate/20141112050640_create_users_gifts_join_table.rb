a6c5261726570c552d760d9b9d1e084da20c08ed
yohanyi <yohanyista@gmail.com>
2014-11-15 00:57:28 -0800

    deleted"

--- db/migrate/20141112050640_create_users_gifts_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsersGiftsJoinTable < ActiveRecord::Migration
-  def change
-    create_table :gifts_users do |t|
-      t.belongs_to :user
-      t.belongs_to :gift
-    end
-  end
-end
